6c730a3603b44ecee224aa811860255ba0f10f2e
David Paschich <dpassage@flophouse.com>
2011-02-09 21:30:05 -0800

    Brand new Rails app before any changes

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bayougrandprix::Application.config.secret_token = 'a2b73ab13eb555abc5e7bf4317f5923c14cc4cd442a4add9fc2b57a7b55f42a05665aa93cedded027d3116aaa772c6f62388d5181c8e6b8a60de2e7a7ae2af20'
