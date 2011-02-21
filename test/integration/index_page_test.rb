d3e190baffff5f3776a422a59f6d39bc41d0df4f
David Paschich <dpassage@flophouse.com>
2011-02-20 17:30:22 -0800

    tests for index page

--- test/integration/index_page_test.rb
+++ test/integration/index_page_test.rb
@@ -1,10 +1,18 @@
 require 'test_helper'
 
 class IndexPageTest < ActionDispatch::IntegrationTest
-  fixtures :all
 
   # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test "load index page and go to season editing" do
+    get "/"
+    
+    assert_response :success
+    # make sure there is one row for each season, plus the title row
+    assert_select "table" do
+      assert_select "tr", Season.count + 1
+    end
+    
+    # make sure there's a like to edit seasons
+    assert_select "a[href=/seasons]"
   end
 end
