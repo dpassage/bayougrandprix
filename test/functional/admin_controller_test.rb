e3e9d6fd001d95e20ebabee9c36a89358553ba1e
David Paschich <dpassage@flophouse.com>
2011-02-23 21:05:26 -0800

    moved single-page test from integratino to functional

--- test/functional/admin_controller_test.rb
+++ test/functional/admin_controller_test.rb
@@ -1,8 +1,18 @@
 require 'test_helper'
 
 class AdminControllerTest < ActionController::TestCase
+  fixtures :all
+  
   # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test "load admin page" do
+    get :index
+    assert_response :success
+    
+    # make sure there's a link to edit seasons
+    assert_select "a[href=/seasons]"
+    # and to edit players
+    assert_select "a[href=/players]"
+    # and to edit drivers
+    assert_select "a[href=/drivers]"
   end
 end
