ca4b5b62a8ca750ebc573324d23236836b78a722
David Paschich <dpassage@flophouse.com>
2011-02-23 22:01:32 -0800

    sort seasons by name on admin page

--- test/functional/admin_controller_test.rb
+++ test/functional/admin_controller_test.rb
@@ -1,4 +1,5 @@
 require 'test_helper'
+require 'ruby_debug'
 
 class AdminControllerTest < ActionController::TestCase
   fixtures :all
@@ -7,12 +8,18 @@ class AdminControllerTest < ActionController::TestCase
   test "load admin page" do
     get :index
     assert_response :success
-    
+
     # make sure there's a link to edit seasons
     assert_select "a[href=/seasons]"
     # and to edit players
     assert_select "a[href=/players]"
     # and to edit drivers
     assert_select "a[href=/drivers]"
+
+    #list should contain seasons in order
+    assert_select "tr:nth-of-type(2) td", "2001"
+    assert_select "tr:nth-of-type(3) td", "2002"
+    assert_select "tr:nth-of-type(4) td", "2003"
+    assert_select "tr:nth-of-type(5) td", "2004"
   end
 end
