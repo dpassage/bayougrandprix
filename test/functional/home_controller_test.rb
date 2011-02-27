ebb2c10bc96323e94fe643515eb8506a4bf82e92
David Paschich <dpassage@flophouse.com>
2011-02-27 14:11:59 -0800

    more complete home page season list test

--- test/functional/home_controller_test.rb
+++ test/functional/home_controller_test.rb
@@ -7,9 +7,9 @@ class HomeControllerTest < ActionController::TestCase
 
     #list should contain seasons in order, each which is a link to a results page
     assert_select "tr:nth-of-type(2) td a[href=/seasons/2001/results]"
-    assert_select "tr:nth-of-type(3) td", "2002"
-    assert_select "tr:nth-of-type(4) td", "2003"
-    assert_select "tr:nth-of-type(5) td", "2004"
+    assert_select "tr:nth-of-type(3) td a[href=/seasons/2002/results]"
+    assert_select "tr:nth-of-type(4) td a[href=/seasons/2003/results]"
+    assert_select "tr:nth-of-type(5) td a[href=/seasons/2004/results]"
   end
 
 end
