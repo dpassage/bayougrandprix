fb7c21a26bfcec1fe216de4fe079f6cdb824a8a2
David Paschich <dpassage@flophouse.com>
2011-05-17 20:50:53 -0700

    add some validation to season_entry model

--- app/models/season_entry.rb
+++ app/models/season_entry.rb
@@ -2,4 +2,7 @@ class SeasonEntry < ActiveRecord::Base
   belongs_to :driver
   belongs_to :season
   belongs_to :defaultteam, :class_name => "Team", :foreign_key => "defaultteam"
+  validates_presence_of :defaultteam
+  validates_presence_of :driver
+  validates_presence_of :season
 end
--- test/unit/season_entry_test.rb
+++ test/unit/season_entry_test.rb
@@ -1,4 +1,23 @@
 require 'test_helper'
 
 class SeasonEntryTest < ActiveSupport::TestCase
+  fixtures :all
+  test "season entry must have a season" do
+    se = season_entries(:season_entries_036)
+    se.season = nil
+    assert !se.save
+  end
+  test "season entry must have a season, team, and driver" do
+    se = SeasonEntry.new
+    se.season = nil
+    se.defaultteam = nil
+    se.driver = nil
+    assert !se.save
+    se.season = seasons(:season_2002)
+    assert !se.save
+    se.defaultteam = teams(:mclaren)
+    assert !se.save
+    se.driver = drivers(:mika)
+    assert se.save
+  end
 end
