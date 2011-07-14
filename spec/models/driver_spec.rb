a0b5fe1f8f58404dc29b2e97ea0885204c874e83
David Paschich <dpassage@flophouse.com>
2011-07-14 14:04:22 -0700

    added spec for driver model

---
+++ spec/models/driver_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe Driver do
+  describe "#default_team_for_season" do
+    it "returns the default team for that driver in that season" do
+      scheme = ScoringScheme.create!(:name => "2-1")
+      season = Season.create!(:name => "1973", :scoring_scheme => scheme)
+      player = Player.create!(:name => "David")
+      driver = Driver.create!(:name => "David", :player => player)
+      team = Team.create!(:name => "Mclaren", :color => Team::Colors["Blue"])
+      season_entry = SeasonEntry.create!( 
+        :season => season,
+        :driver => driver,
+        :defaultteam => team
+      )
+      driver.default_team_for_season(season).should == team
+    end
+  end
+end
