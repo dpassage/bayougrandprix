6d3f7e10e9b08f8ae810e95e8c712f72454d49c4
David Paschich <dpassage@flophouse.com>
2011-07-14 15:59:55 -0700

    fill out team examples

--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -1,11 +1,33 @@
 require 'spec_helper'
 
 describe Team do
-  it "is valid with valid parameters"
-  it "is not valid without a name"
-  it "is not valid without a color"
-  it "is not valid with an invalid color"
-  it "has a hash of valid colors"
-  it "is not valid if fake is not set"
-  it "is not valid if fake is not true or false"
+  before(:each) do
+    @team = Team.new(:name => "McLaren", :color => Team::Colors["Blue"])
+  end
+  it "is valid with valid parameters" do
+    @team.should be_valid
+  end
+  it "is not valid without a name" do
+    @team.name = nil
+    @team.should_not be_valid
+  end
+  it "is not valid without a color" do
+    @team.color = nil
+    @team.should_not be_valid
+  end
+  it "is not valid with an invalid color" do
+    @team.color = "This isn't an HTML color code"
+    @team.should_not be_valid
+  end
+  it "has a hash of valid colors" do
+    Team::Colors.should_not == nil
+    Team::Colors["Blue"].should_not == nil
+  end
+  it "is not fake by default" do
+    @team.fake.should_not == true
+  end
+  it "is not valid if fake is not set" do
+    @team.fake = nil
+    @team.should_not be_valid
+  end
 end
