52c89e27cd38af1ce39371ea9365a20894617551
David Paschich <dpassage@flophouse.com>
2011-07-14 14:10:40 -0700

    spec for seasons helper

---
+++ spec/helpers/seasons_helper_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+# Specs in this file have access to a helper object that includes
+# the HomeHelper. For example:
+#
+# describe HomeHelper do
+#   describe "string concat" do
+#     it "concats two strings with spaces" do
+#       helper.concat_strings("this","that").should == "this that"
+#     end
+#   end
+# end
+describe SeasonsHelper do
+  describe "#ordinal" do
+    it "returns Winner for 1" do
+      helper.ordinal(1).should == 'Winner'
+    end
+    it "returns 2nd for 2" do
+      helper.ordinal(2).should == '2nd'
+    end
+    it "returns 3rd for 3" do
+      helper.ordinal(3).should == '3rd'
+    end
+    it "returns 4th for 4" do
+      helper.ordinal(4).should == '4th'
+    end
+  end
+end
