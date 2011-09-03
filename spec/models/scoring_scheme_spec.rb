1cdc59522b5912276a57b66a220974d9ef9785b6
David Paschich <dpassage@flophouse.com>
2011-09-02 17:24:43 -0700

    spec for scoring scheme remove now redundant test

--- spec/models/scoring_scheme_spec.rb
+++ spec/models/scoring_scheme_spec.rb
@@ -3,13 +3,30 @@ require 'spec_helper'
 describe "ScoringScheme" do
   describe "#points_for_finishing" do
     context "with a scheme named 9-6-4-3-2-1" do
-      it "returns 9 for 1st"
-      it "returns 6 for 2nd"
-      it "returns 4 for 3rd"
-      it "returns 3 for 4th"
-      it "returns 2 for 5th"
-      it "returns 1 for 6th"
-      it "returns 0 for 7th"
+      before(:each) do
+        @scheme = ScoringScheme.new(:name => "9-6-4-3-2-1")
+      end
+      it "returns 9 for 1st" do
+        @scheme.points_for_finishing(1).should == 9
+      end
+      it "returns 6 for 2nd" do
+        @scheme.points_for_finishing(2).should == 6
+      end
+      it "returns 4 for 3rd" do
+        @scheme.points_for_finishing(3).should == 4
+      end
+      it "returns 3 for 4th" do
+        @scheme.points_for_finishing(4).should == 3
+      end
+      it "returns 2 for 5th" do
+        @scheme.points_for_finishing(5).should == 2
+      end
+      it "returns 1 for 6th" do
+        @scheme.points_for_finishing(6).should == 1
+      end
+      it "returns 0 for 7th" do
+        @scheme.points_for_finishing(7).should == 0
+      end
     end
   end
 end
