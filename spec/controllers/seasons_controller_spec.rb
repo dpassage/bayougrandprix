b6efa625496e9e8c6005525a6bbbe8677eebbf9a
David Paschich <dpassage@flophouse.com>
2012-06-08 09:57:13 -0700

    trying to make seasons controller spec like other controller specs more work to be done...

--- spec/controllers/seasons_controller_spec.rb
+++ spec/controllers/seasons_controller_spec.rb
@@ -2,64 +2,86 @@ require 'spec_helper'
 
 describe SeasonsController do
   def user_is_admin
-    @request.session[:role] = "admin"
+    controller.stub(:admin?).and_return(true)
   end
 
   def user_is_guest
-    @request.session[:role] = "guest"
+    controller.stub(:admin?).and_return(false)
   end
   
   describe "POST create" do
     let(:name) { "2035" }
-    let(:scoringscheme) { mock_model('ScoringScheme') }
-    let(:season) { mock_model('Season').as_null_object }
-    let(:create_params) { { }}
+    let(:ss) { FactoryGirl.create(:scoring_scheme) }
+    let(:create_params) { { season: { name: name, scoring_scheme: ss.to_param } } }
     context "when the user is not an admin" do
       before (:each) do 
         user_is_guest
       end
       it_should_behave_like "an unauthorized operation" do
         before (:each) do
-          post :create, :season => { "name" => "2035", "scoring_scheme" => scoringscheme }
+          post :create, create_params
         end
       end
     end
     context "when user is admin" do
       before (:each) do
         user_is_admin
-        Season.stub(:new).and_return(season)
-        ScoringScheme.stub(:find).and_return(scoringscheme)
       end
-      it "creates a new season" do
-        Season.should_receive(:new).with(:name => name, 
-                                         :scoring_scheme => scoringscheme )
-                                   .and_return(season)
-        post :create, :season => { "name" => "2035", "scoring_scheme" => scoringscheme }
+      context "with valid params" do
+        it "creates a new season" do
+          expect {
+            post :create, create_params
+          }.to change(Season, :count).by(1)
+        end
+        it "redirects to index" do
+          post :create, create_params
+          response.should redirect_to(seasons_path)
+        end
       end
-      it "saves the season" do
-        season.should_receive(:save)
-        post :create, :season => { "name" => "2035", "scoring_scheme" => scoringscheme }
-      end      
-      it "redirects to index" do
-        post :create, :season => { "name" => "2035", "scoring_scheme" => scoringscheme }
-        response.should redirect_to(seasons_path)
+      context "with invalid name" do
+        let(:invalid_params) { { "season" => { "name" => nil, "scoring_scheme" => ss.to_param } } }
+        it "does not create a new season" do
+          expect {
+            post :create, invalid_params
+          }.to change(Season, :count).by(0)
+        end
+        # it "sets the error flash" do
+        #   post :create, invalid_params
+        #   flash[:error].should_not be_nil
+        # end
+        it "renders the new page" do
+          post :create, invalid_params
+          response.should render_template("new")
+        end
       end
+      # context "with invalid scheme" do
+      #   let(:invalid_params) { { "season" => { "name" => "name", "scoring_scheme_id" => ss.to_param + "4" } } }
+      #   it "does not create a new season" do
+      #     expect {
+      #       post :create, invalid_params
+      #     }.to change(Season, :count).by(0)
+      #   end
+      #   it "sets the error flash" do
+      #     post :create, invalid_params
+      #     flash[:error].should_not be_nil
+      #   end
+      #   it "renders the new page" do
+      #     post :create, invalid_params
+      #     response.should render_template("new")
+      #   end
+      # end
     end
   end
   describe "GET show" do
-    before(:each) do 
-      @season = mock_model('Season').as_null_object
-      Season.stub(:find_by_name).and_return( @season ) 
-    end
+    let(:season) { FactoryGirl.create(:season) }
+    let(:show_params) { { id: season.to_param } }
     shared_examples "always passes some variables" do
       it "should pass an array of season entries" do
-        @season.should_receive(:season_entries).and_return( [ ] )
-        get 'show'
+        get 'show', show_params
         assigns[:season_entries].should_not == nil
       end
       it "should pass a list of races in the season" do
-        @season.should_receive(:races).and_return ( [ ] )
-        get 'show'
+        get 'show', show_params
         assigns[:races].should_not == nil
       end      
     end
@@ -67,13 +89,11 @@ describe SeasonsController do
       before (:each) { user_is_admin }
       include_examples "always passes some variables"
       it "should pass an array of drivers" do
-        Driver.should_receive(:order).and_return ( [ ] )
-        get 'show'
+        get 'show', show_params
         assigns[:drivers].should_not == nil
       end
       it "should pass a list of all tracks" do
-        Track.should_receive(:all).and_return ( [ ] )
-        get 'show'
+        get 'show', show_params
         assigns[:tracks].should_not == nil
       end
     end
@@ -81,31 +101,32 @@ describe SeasonsController do
       before (:each) { user_is_guest }
       include_examples "always passes some variables"
       it "should not pass an array of drivers" do
-        get 'show'
+        get 'show', show_params
         assigns[:drivers].should be_nil
       end
       it "should not pass a list of all tracks" do
-        get 'show'
+        get 'show', show_params
         assigns[:tracks].should be_nil
       end
     end
   end
   describe "GET results" do
     let(:season) { FactoryGirl.create(:season) }
+    let(:results_params) { { "id" => season.to_param } }
     it "passes the season to the view" do
-      get 'results', :id => season.name
+      get 'results', results_params
       assigns[:season].should == season
     end
     it "passes drivers by points to the view" do
-      get 'results', :id => season.name
+      get 'results', results_params
       assigns[:drivers_by_points].should_not be_nil
     end
     it "passes teams by points to the view" do
-      get 'results', :id => season.name
+      get 'results', results_params
       assigns[:teams_by_points].should_not be_nil
     end
     it "passes drivers by qualifying points to the view" do
-      get 'results', :id => season.name
+      get 'results', results_params
       assigns[:drivers_by_qualifying_points].should_not be_nil
     end
   end
