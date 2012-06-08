427a428cb6f3d092e206f2a39571e7d18b2887a5
David Paschich <dpassage@flophouse.com>
2012-06-08 11:57:28 -0700

    turns out, you don't need special validation to make sure the player object exists..

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,17 +1,10 @@
 class Driver < ActiveRecord::Base
   validates_presence_of :name
+  validates_presence_of :player
   belongs_to :player
   has_many :season_entries, :dependent => :restrict
   has_many :race_entries
   attr_accessible :name, :player_id
-  validate :player_must_exist
-  
-  def player_must_exist
-    if !Player.exists?(player_id)
-      errors.add(:player_id, "must exist")
-    end
-  end
-  
 
   def default_team_for_season(season)
     self.season_entries.where(:season_id => season.id).first.defaultteam
