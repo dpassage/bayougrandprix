19f6343036505c2b38763798d0c7cfb869ed515b
David Paschich <dpassage@flophouse.com>
2012-06-12 01:08:39 -0700

    show players

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -5,6 +5,9 @@ class PlayersController < ApplicationController
   def index
     @players = Player.all
   end
+  def show
+    @player = Player.find(params[:id])
+  end
   def new
     @player = Player.new
   end
--- spec/controllers/players_controller_spec.rb
+++ spec/controllers/players_controller_spec.rb
@@ -7,6 +7,21 @@ describe PlayersController do
   describe "GET 'new'" do
     it_should_behave_like "standard new CRUD", :player
   end
+  describe "GET show" do
+    let(:player) { FactoryGirl.create(:player) }
+    it "should be successful for a real driver" do
+      get 'show', :id => player.to_param
+      response.should be_success
+    end
+    it "should render the show template" do
+      get 'show', :id => player.to_param
+      response.should render_template("show")
+    end
+    it "should pass the driver to the template" do
+      get 'show', :id => player.to_param
+      assigns[:player].should == player
+    end
+  end
   describe "POST 'create'" do
     let(:create_params) { { player: { name: "Joe", email: "joe@example.com" } } }
     let(:invalid_params) { { team: {}}}
