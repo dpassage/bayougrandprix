c35d4cf8da9a2ec8b3e58a0090eb8815e6bd4132
David Paschich <dpassage@flophouse.com>
2012-07-13 11:15:46 -0700

    Use Markdown to render race writeups. next: figuring out how to extend markdown to more easily handle mid-race results tables.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
+require 'redcarpet'
+
 module ApplicationHelper
+  def markdown_render(text)
+    @renderer ||= Redcarpet::Markdown.new(Redcarpet::Render::HTML)
+    @renderer.render(text)
+  end
 end
