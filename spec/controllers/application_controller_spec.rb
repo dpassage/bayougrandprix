b1f51a7ac17e0b9490a4fd8b8c238404b80a570c
David Paschich <dpassage@balveda.com>
2013-06-27 17:13:53 -0700

    start cleaning up specs...

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -1,55 +1,55 @@
 require 'spec_helper'
 
 describe ApplicationController do
-  describe "#authorize" do
-    context "when the user is the admin" do
+  describe '#authorize' do
+    context 'when the user is the admin' do
       before (:each) do
         user_is_admin
       end
-      it "returns true" do
+      it 'returns true' do
         result = controller.authorize
         result.should be_true
       end
-      it "does not set the error flash" do
-        result = controller.authorize
+      it 'does not set the error flash' do
+        controller.authorize
         flash[:error].should be_nil
       end
     end
-    context "when the user is not the admin" do
+    context 'when the user is not the admin' do
       before (:each) do
         user_is_guest
         controller.stub(:redirect_to)
       end
-      it "returns false" do
+      it 'returns false' do
         result = controller.authorize
         result.should be_false
       end
-      it "sets the error flash" do
-        result = controller.authorize
+      it 'sets the error flash' do
+        controller.authorize
         flash[:error].should_not be_nil
       end
-      it "redirects to the root path" do
+      it 'redirects to the root path' do
         controller.should_receive(:redirect_to).with(root_path)
         controller.authorize
       end
     end
   end
-  describe "#admin?" do
-    it "returns true when the session role is admin" do
+  describe '#admin?' do
+    it 'returns true when the session role is admin' do
       session[:role] = 'admin'
       controller.admin?.should be_true
     end
-    it "returns false when the session role is something else" do
+    it 'returns false when the session role is something else' do
       session[:role] = 'joe bob'
       controller.admin?.should be_false
     end
   end
-  describe "#logged_in_as" do
-    it "returns 'admin' when the user is an admin" do
+  describe '#logged_in_as' do
+    it 'returns \'admin\' when the user is an admin' do
       user_is_admin
       controller.logged_in_as.should == 'admin'
     end
-    it "returns 'guest' when the user is not an admin" do
+    it 'returns \'guest\' when the user is not an admin' do
       user_is_guest
       controller.logged_in_as.should == 'guest'
     end
--- spec/controllers/teams_controller_spec.rb
+++ spec/controllers/teams_controller_spec.rb
@@ -3,155 +3,155 @@ require 'spec_helper'
 describe TeamsController do
   let (:team) { FactoryGirl.create(:team) }
   let (:params) { { id: team.to_param } }
-  describe "GET index" do
-    it_should_behave_like "standard index CRUD", :teams
+  describe 'GET index' do
+    it_should_behave_like 'standard index CRUD', :teams
   end
-  describe "GET 'new'" do
-    it_should_behave_like "standard new CRUD", :team
+  describe 'GET new' do
+    it_should_behave_like 'standard new CRUD', :team
   end
-  describe "GET 'show'" do
+  describe 'GET show' do
     before(:each) do
       get 'show', params
     end
-    it ("should be successful") { response.should be_success }
-    it ("should render the show template") {
-      response.should render_template("show") }
-    it ("should pass the team") { assigns[:team].should == team }
+    it ('should be successful') { response.should be_success }
+    it ('should render the show template') {
+      response.should render_template('show') }
+    it ('should pass the team') { assigns[:team].should == team }
   end
-  describe "GET 'edit'" do
+  describe 'GET edit' do
     before(:each) do
       get 'edit', params
     end
-    it ("should be successful") { response.should be_success }
-    it ("should render the edit template") {
-      response.should render_template("edit") }
-    it ("should pass the team") { assigns[:team].should == team }
+    it ('should be successful') { response.should be_success }
+    it ('should render the edit template') {
+      response.should render_template('edit') }
+    it ('should pass the team') { assigns[:team].should == team }
   end
-  describe "POST 'update'" do
-    let (:update_params) { { "id"=> team.to_param,
-                             "team"=>{"name"=>"Foo!",
-                                      "color"=>team.color,
-                                      "fake"=>team.fake } } }
-    context "the user is not an admin" do
+  describe 'POST update' do
+    let (:update_params) { { 'id'=> team.to_param,
+                             'team'=>{'name'=>'Foo!',
+                                      'color'=>team.color,
+                                      'fake'=>team.fake } } }
+    context 'the user is not an admin' do
       before(:each) do
         user_is_guest
       end
-      it_should_behave_like "an unauthorized operation" do
+      it_should_behave_like 'an unauthorized operation' do
         before(:each) do
           post 'update', update_params
         end
       end
-      it ("should not change the team") do
+      it ('should not change the team') do
         post 'update', update_params
-        team.name.should_not == "Foo!"
+        team.name.should_not == 'Foo!'
       end
     end
-    context "the user is an admin" do
+    context 'the user is an admin' do
       before(:each) { user_is_admin }
-      context "with valid params" do
+      context 'with valid params' do
         before(:each) { post 'update', update_params }
-        it("should redirect to the show template") {
+        it('should redirect to the show template') {
           response.should redirect_to(team_path(team)) }
-        it("should change the team name") {
-          Team.find(team.id).name.should == "Foo!" }
+        it('should change the team name') {
+          Team.find(team.id).name.should == 'Foo!' }
       end
     end
   end
-  describe "POST 'create'" do
-    let (:create_params) { { team: { name: "New Team",
-                                     color: Team::COLORS["Pink"],
+  describe 'POST create' do
+    let (:create_params) { { team: { name: 'New Team',
+                                     color: Team::COLORS['Pink'],
                                      fake: false } } }
     let(:invalid_params) { { team: {}}}
-    describe "when user is not an admin" do
+    describe 'when user is not an admin' do
       before(:each) do
         user_is_guest
       end
-      it_should_behave_like "an unauthorized operation" do
+      it_should_behave_like 'an unauthorized operation' do
         before(:each) do
           post 'create', create_params
         end
       end
-      it "should not create the team" do
+      it 'should not create the team' do
         expect {
           post 'create', create_params
         }.to change(Team, :count).by(0)
       end
     end
-    describe "when the user is an admin" do
+    describe 'when the user is an admin' do
       before(:each) do
         @create_params = create_params
         @teams_path = teams_path
         user_is_admin
       end
-      it_should_behave_like "standard create CRUD" do
+      it_should_behave_like 'standard create CRUD' do
         let(:klass) { Team }
         let(:redirect_path) { teams_path }
       end
-      describe "with invalid parameters" do
-        it "renders the new template" do
+      describe 'with invalid parameters' do
+        it 'renders the new template' do
           post 'create', invalid_params
-          response.should render_template("new")
+          response.should render_template('new')
         end
       end
     end
   end
-  describe "DELETE 'destroy'" do
-    let (:delete_params) { { "id" => team.to_param } }
-    context "when the user is not an admin" do
+  describe 'DELETE destroy' do
+    let (:delete_params) { { 'id' => team.to_param } }
+    context 'when the user is not an admin' do
       before(:each) {
         user_is_guest
       }
-      it_should_behave_like "an unauthorized operation" do
+      it_should_behave_like 'an unauthorized operation' do
         before(:each) do
           delete 'destroy', delete_params
         end
       end
-      it("should not delete the team") do
+      it('should not delete the team') do
         delete 'destroy', delete_params
         Team.find(team.id).should_not be_nil
       end
     end
-    context "when the user is an admin" do
+    context 'when the user is an admin' do
       before(:each) do
         user_is_admin
       end
-      context "when the team is not used" do
+      context 'when the team is not used' do
         before (:each) do
           delete 'destroy', delete_params
         end
-        it("deletes the team") {
+        it('deletes the team') {
           expect {
             Team.find(team.id)
           }.to raise_error ActiveRecord::RecordNotFound
         }
-        it("redirects to teams page") {
+        it('redirects to teams page') {
           response.should redirect_to(teams_path)
         }
-        it("sets the notify flash") {
+        it('sets the notify flash') {
           flash[:notice].should_not be_nil
         }
       end
-      context "when the team is used in a driver_entry" do
+      context 'when the team is used in a driver_entry' do
         before (:each) do
           de = FactoryGirl.create(:driver_entry, defaultteam: team)
           delete 'destroy', delete_params
         end
-        it("is not deleted") { Team.find(team.id).should == team }
-        it("redirects to teams page") {
+        it('is not deleted') { Team.find(team.id).should == team }
+        it('redirects to teams page') {
           response.should redirect_to(teams_path) }
-        it("sets the error flash") { flash[:error].should_not be_nil }
+        it('sets the error flash') { flash[:error].should_not be_nil }
       end
-      context "when the team is used in a race_entry" do
+      context 'when the team is used in a race_entry' do
         before (:each) do
           otherteam = FactoryGirl.create(:team)
           de = FactoryGirl.create(:driver_entry, defaultteam: otherteam)
           re = FactoryGirl.create(:race_entry, driver_entry: de, team: team)
           delete 'destroy', delete_params
         end
-        it("is not deleted") { Team.find(team.id).should == team }
-        it("redirects to teams page") {
+        it('is not deleted') { Team.find(team.id).should == team }
+        it('redirects to teams page') {
           response.should redirect_to(teams_path) }
-        it("sets the error flash") { flash[:error].should_not be_nil }
+        it('sets the error flash') { flash[:error].should_not be_nil }
       end
     end
   end
